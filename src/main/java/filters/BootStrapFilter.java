e986656315b6344bf4cd944c5a218dab44832725
acornUdada <tlstkd153@naver.com>
2023-12-20 18:48:22 +0900

    BootStrap 필터 기능 수정

--- src/main/java/filters/BootStrapFilter.java
+++ src/main/java/filters/BootStrapFilter.java
@@ -5,26 +5,37 @@ import java.io.PrintWriter;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
 
 @WebFilter("/*")
 public class BootStrapFilter implements Filter {
-	@Override
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-			throws IOException, ServletException {
-		Boolean bootstrapAdded = (Boolean) request.getAttribute("bootstrapAdded");
-		
-		if (bootstrapAdded == null || !bootstrapAdded) {
-			PrintWriter out = response.getWriter();
-            out.println("<link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css\">");
-            out.println("<script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL\" crossorigin=\"anonymous\"></script>");
 
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        Boolean bootstrapAdded = (Boolean) request.getAttribute("bootstrapAdded");
+
+        if (bootstrapAdded == null || !bootstrapAdded) {
+            HttpServletResponse httpResponse = (HttpServletResponse) response;
+            httpResponse.getWriter().println("<link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css\">");
+            httpResponse.getWriter().println("<script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL\" crossorigin=\"anonymous\"></script>");
             request.setAttribute("bootstrapAdded", true);
-	     }
+        }
+        chain.doFilter(request, response);
+    }
 
-	     chain.doFilter(request, response);
-	}
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // 초기화 코드
+    }
+
+    @Override
+    public void destroy() {
+        // 필터 소멸 시 호출되는 코드
+    }
 }
