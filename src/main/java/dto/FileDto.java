db38f5a2bebd17215798733cfed3802640d472d6
sonsanghee <sonsanghee3@gmail.com>
2023-12-20 16:39:49 +0900

    zz

--- src/main/java/dto/FileDto.java
+++ src/main/java/dto/FileDto.java
@@ -10,11 +10,10 @@ import lombok.Setter;
 @Getter
 @Setter
 public class FileDto {
-	private int num;
-	private String writer; //로그인 사용자의 아이디
-	private String title;
-	private String orgFileName;
-	private String saveFileName;
-	private long fileSize;
-	private String regdate;
+	private int upload_id;
+	private String uploader_id; //로그인 사용자의 아이디
+	private String org_filename;
+	private String save_filename;
+	private long filesize;
+	private String uploaded_at;
 }
